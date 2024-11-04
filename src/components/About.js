7fd01885948b6782a72674cd91f93c850d5967fa
kavmal7 <kavyamal74@gmail.com>
2024-11-05 00:10:41 +0100

    Changed about

--- src/components/About.js
+++ src/components/About.js
@@ -16,13 +16,13 @@ const About = () => (
             
             <Box flex="2" textAlign="justify" ml={12} width="60%">
                 <Text fontSize="lg" mb={4}>
-                    Hi, my name is Kavya Malhotra and welcome to my online CV. I am a bachelor's graduate in Data Science with experience in retail, web development, and coding. My experiences in my profession and group projects at my university have helped me become a good communicator and team player.
+                    Hi, my name is Kavya Malhotra and welcome to my online CV. I am a bachelor's graduate in Data Science with experience in retail and development. My experiences in my profession and group projects at my university have helped me become a good communicator and team player, working with people from many parts of the world.
                 </Text>
                 <Text fontSize="lg" mb={4}>
-                    I picked Data Science as I have always been interested in topics such as Artificial Intelligence and its potential has always fascinated me. Data Science is a key field that encompasses AI and Machine Learning, both of which are my key interests. I am an eager learner with a high passion for the field of IT.
+                    I picked Data Science as I have always been interested in topics such as Artificial Intelligence and its potential has always fascinated me. Data Science is a key field that has a significant overlap with AI and Machine Learning, both of which are my key interests.
                 </Text>
                 <Text fontSize="lg" mb={4}>
-                    Even though I specialise in Data Science, my keen interest in computers has encouraged me to take extra courses to widen my scope in the realm of computer science (for example, computer networking and operating systems). In addition to this, I have also taken online courses, including but not limited to C, Python, SQL, JavaScript, etc to deepen my understanding and working knowledge in this field, which I have been able to apply professionally as well.
+                    Even though I specialise in Data Science, my keen interest in computers has encouraged me to take extra courses to widen my scope in the realm of computer science (for example, computer networking and operating systems). In addition to this, I have also taken online courses, from courses on various programming languages to courses about more in-depth topics such as reinforcement learning. This has deepened my understanding and working knowledge in these areas, which I have been (to a large extent) able to apply professionally as well.
                 </Text>
                 <Text fontSize="lg" mb={4}>
                     In addition to the aforementioned, I am able to speak multiple languages. I am fluent in English, Norwegian, and Hindi, but also have a rudimentary understanding of other languages such as French.
